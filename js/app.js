cfecdb9aa54861accdc16d43241e3f68c11909f3
DmitryMelkov <92420054+DmitryMelkov@users.noreply.github.com>
2021-12-07 13:50:51 +0500

    add corrections

--- js/app.js
+++ js/app.js
@@ -156,47 +156,4 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-
-  //эмуляция работы аккордиона с сервером
-  //это json  который нам пришел с сервера
-  const contentData = [
-    {
-      id: 1,
-      title: "title1",
-      content: "content1",
-    },
-    {
-      id: 2,
-      title: "title2",
-      content: "content2",
-    },
-    {
-      id: 3,
-      title: "title3",
-      content: "content3",
-    },
-    {
-      id: 4,
-      title: "title4",
-      content: "content4",
-    },
-    {
-      id: 5,
-      title: "title5",
-      content: "content5",
-    },
-  ];
-
-  const createTemplate = (item) => {
-    return `
-    <div class="accordion__item">
-      <div class="accordion__item-header">
-        <h3 class="accordion__item-title">${item.title}</h3>
-        <div class="accordion__item-btn"></div>
-      </div>
-      <div class="accordion__item-content">${item.content}</div>
-    </div>
-
-    `;
-  };
 });
